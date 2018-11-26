488bdb840abf846c64608176a6cfe51c1ddf4c0d
Marvin Lin <marvin.lin@cloud-interactive.com>
2018-11-26 17:20:13 +0800

    [Add] Build Phases add for different environment

--- CIAppKit/CIAppKit/CIAppKit.podspec
+++ CIAppKit/CIAppKit/CIAppKit.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "CIAppKit"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "Collection of useful tools for iOS app development."
 
   # This description is used to generate tags and improve search results.
---
+++ CIAppKit/CIAppKit/Products/CIAppKit.framework/Headers/CIAppKit.h
@@ -0,0 +1,20 @@
+//
+//  CIAppKit.h
+//  CIAppKit
+//
+//  Created by Moon on 2018/11/26.
+//  Copyright © 2018 MobileTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CIAppKit.
+FOUNDATION_EXPORT double CIAppKitVersionNumber;
+
+//! Project version string for CIAppKit.
+FOUNDATION_EXPORT const unsigned char CIAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CIAppKit/PublicHeader.h>
+
+#import <CIAppKit/CIFormatter.h>
+#import <CIAppKit/CINumberFormatter.h>
